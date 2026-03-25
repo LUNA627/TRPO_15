b2ee2d92b2b977f18fd6ce128fae59ec2d14e51b
LUNA627 <say.samurai@yandex.ru>
2026-03-25 14:41:56 +0500

    first commit

---
+++ ElectronicStoreShop/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElectronicStoreShop.Models;
+
+public partial class Tag
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+
+}
